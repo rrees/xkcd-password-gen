cff0bb3813cff06eb6323500c5644c05d7588806
Robert Rees <robert@wazoku.com>
2011-09-24 14:41:18 +0100

    Working password generator

--- password.rb
+++ password.rb
@@ -1,4 +1,22 @@
+require 'set'
 
 def passwords()
-	return ["Badger", "Badger", "Badger", "Badger"]
+	passwords = Set.new
+	until passwords.size() == 4 do
+		passwords.add(password())
+	end
+	return passwords
 end
+
+
+def password()
+	password_tokens = [
+		["badger", "dog", "frog", "butterfly"],
+		["happy", "sad", "lonely", "funny"],
+		["battery", "staple", 'truck', 'pylon'],
+		["cheese", "carrot", "soup", "liquor"],
+		["starlit", "darkness", "frosty"],
+	]
+	return password_tokens.sample().sample()
+end
+
