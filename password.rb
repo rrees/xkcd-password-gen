0f5ef4d5b21544e10a21bc2a9ffb87ca578d2b3a
Robert Rees <robert@wazoku.com>
2011-09-25 09:38:51 +0100

    Adding more words

--- password.rb
+++ password.rb
@@ -12,15 +12,16 @@ end
 def password()
 	password_tokens = [
 		["badger", "dog", "frog", "butterfly", "cat", "camel", "mouse", "fish", "shark", 'koala', 'bat'],
-		["happy", "sad", "lonely", "funny", "crying", "mean"],
-		["battery", "staple", 'screwdriver', 'pylon', 'book', 'cell'],
+		["happy", "sad", "lonely", "funny", "crying", "mean", "stern", "fantastic"],
+		["battery", "staple", 'screwdriver', 'pylon', 'book', 'cell', 'hammer', 'torch', 'book'],
 		["cheese", "carrot", "soup", "liquor", "sausage", "treacle", "nuts"],
 		["starlit", "darkness", "frosty"],
-		["truck", "car", "bicycle", "train", "plane", "boat"],
+		["truck", "car", "bicycle", "train", "plane", "boat", 'taxi'],
 		["tree", "flower", "cactus"],
-		['theatre', 'museum', 'laundry', 'house', 'hut'],
+		['theatre', 'museum', 'laundry', 'house', 'hut', 'bank', 'park', 'garden'],
 		['lime', 'lemon', 'mango', 'grapefruit', 'orange', 'apple'],
-		['sailor', 'doctor', 'driver', 'fireman', 'conductor']
+		['sailor', 'doctor', 'driver', 'fireman', 'conductor', 'reporter'],
+		['singing', 'dancing', 'running'],
 	]
 	return password_tokens.sample().sample()
 end
