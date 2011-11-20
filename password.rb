a2b5f3b7df44c3547163cf5047008166ec904b54
Robert Rees <robert@wazoku.com>
2011-11-20 19:02:55 +0000

    Adding more words

--- password.rb
+++ password.rb
@@ -11,18 +11,21 @@ end
 
 def password()
 	password_tokens = [
-		["badger", "dog", "frog", "butterfly", "cat", "camel", "mouse", "fish", "shark", 'koala', 'bat'],
-		["happy", "sad", "lonely", "funny", "crying", "mean", "stern", "fantastic"],
-		["battery", "staple", 'screwdriver', 'pylon', 'book', 'cell', 'hammer', 'torch', 'book'],
-		["cheese", "carrot", "soup", "liquor", "sausage", "treacle", "nuts"],
-		["starlit", "darkness", "frosty"],
-		["truck", "car", "bicycle", "train", "plane", "boat", 'taxi'],
-		["tree", "flower", "cactus"],
+		["badger", "dog", "frog", "butterfly", "cat", "camel", "mouse", "fish", "shark", 'bat', 'rhino', "whale"],
+		["happy", "sad", "lonely", "funny", "crying", "mean", "stern", "fantastic", "distant", "intimate", "caring"],
+		["battery", "staple", 'pylon', 'book', 'cell', 'torch', 'book', "floss", "soap"],
+		["spanner", 'screwdriver', 'hammer', 'lathe', 'hacksaw', 'spirit level', "drill", "saw"],
+		["cheese", "carrot", "soup", "liquor", "sausage", "treacle", "nuts", "chocolate"],
+		["starlit", "darkness", "frosty", "blackened", "blue", "still", "fluke", "lonely"],
+		["truck", "car", "bicycle", "train", "plane", "boat", 'taxi', 'coach', 'pickup', "bus", "dodgem"],
+		["tree", "flower", "cactus", "sunflower", "bush", "bay tree", "honeysuckle", "lily", "daffodil"],
 		['theatre', 'museum', 'laundry', 'house', 'hut', 'bank', 'park', 'garden'],
-		['lime', 'lemon', 'mango', 'grapefruit', 'orange', 'apple'],
+		['lime', 'lemon', 'mango', 'grapefruit', 'orange', 'apple', 'blueberry', 'strawberry', 'raspberry'],
 		['sailor', 'doctor', 'driver', 'fireman', 'conductor', 'reporter'],
-		['singing', 'dancing', 'running'],
-		['trumpet', 'horn', 'piano', 'drum'],
+		['singing', 'dancing', 'running', 'skating', 'driving', 'hiding', 'shouting', 'howling'],
+		['trumpet', 'horn', 'piano', 'drum', 'recorder', 'xylophone', 'oboe', 'flute'],
+		['kangaroo', 'wombat', 'koala', 'emu', 'ostrich', 'dingo'],
+		['desert', 'ocean', 'island', 'cape', 'delta', 'fjord', 'tundra', 'plains'],
 	]
 	return password_tokens.sample().sample()
 end
